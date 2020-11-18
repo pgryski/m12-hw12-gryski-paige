fd0567b31cabf4bdfc7329313a09658d8df52832
Paige <paigegryski@gmail.com>
2020-11-17 20:24:05 -0500

    Fixed ES6

--- src/main.js
+++ src/main.js
@@ -10,3 +10,10 @@ var cheese = [
     "American",
     "Pepperjack"
 ]
+
+for (i = points.length -1; i > 0; i--) {
+    j = Math.floor(Math.random() * i)
+    k = points[i]
+    points[i] = points[j]
+    points[j] = k
+  }
